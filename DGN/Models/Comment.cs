acefceb384ad9eb7948d25f54f841fcb0cf73bc8
בר בן יוסף <bar180999@gmail.com>
2021-04-28 21:36:22 +0300

    Adding Comment.cs class props.

---
+++ DGN/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGN.Models
+{
+    public class Comment
+    {
+        // Primery key
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        // Foreign key
+        public User CommentWriter { get; set; }
+
+        // Foreign Key
+        public Article RelatedArticle { get; set; }
+
+    }
+}
