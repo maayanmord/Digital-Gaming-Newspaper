f5804e070230dcafd4828a7b3d874880dd648f17
בר בן יוסף <bar180999@gmail.com>
2021-05-03 09:01:31 +0300

    Adding anutation

--- DGN/Models/Comment.cs
+++ DGN/Models/Comment.cs
@@ -10,13 +10,18 @@ namespace DGN.Models
         // Primery key
         public int Id { get; set; }
 
+        [Required]
+        [StringLenght(20), MinimumLenght = 5]
         public string Title { get; set; }
 
+        [StringLenght(200), MinimumLenght = 5]
         public string Body { get; set; }
 
+        // Shouldnt display in the GET form
         // Foreign key
         public User CommentWriter { get; set; }
 
+        // Shouldnt display in the GET form
         // Foreign Key
         public Article RelatedArticle { get; set; }
 
