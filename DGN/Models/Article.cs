74ef1dacf28f5045efbf4e74968ce1a3c3709e6a
Ran Biderman <bsuro10@gmail.com>
2021-05-03 20:38:37 +0300

    Added Article class

---
+++ DGN/Models/Article.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGN.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [RegularExpression(@"^[A-Z\d].*$", ErrorMessage = "The title must begin with a capital letter or a number")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime CreationTimestamp { get; set; }
+
+        [Required]
+        public DateTime LastUpdatedTimestamp { get; set; }
+
+        public IList<Comment> Comments { get; set; }
+
+        public IList<Profile> Likes { get; set; }
+    }
+}
