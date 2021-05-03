d8105db3b444c7f49a0f20a2bba833d92bc0ca87
Ori Granot <oriorigranot@gmail.com>
2021-05-03 21:18:49 +0300

    Initial Category model

---
+++ DGN/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGN.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Category name")]
+        [Required(AllowEmptyStrings = false)]
+        public string CategoryName { get; set; }
+
+        [DisplayName("The articles that using this category")]
+        public IList<Atricle> ArticlesCategory { get; set; }
+    }
+}
