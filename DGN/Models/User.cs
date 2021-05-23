f46ddc0dca91c4d468d6ed03bd1bb4813cf109d1
בר בן יוסף <bar180999@gmail.com>
2021-05-23 21:17:50 +0300

    FIX: pssssword

--- DGN/Models/User.cs
+++ DGN/Models/User.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DGN.Models
 {
@@ -22,10 +20,9 @@ namespace DGN.Models
         public string UserName { get; set; }
 
         [ForeignKey("Password")]
-        public int PssswordID { get; set; }
+        public int PasswordID { get; set; }
 
         [Required]
-        [StringLength(50, MinimumLength = 5)]
         public Password Password { get; set; }
 
         [DisplayName("The Profile Image Location")]
