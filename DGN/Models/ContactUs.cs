bb4e663b0e0a43027f0f660bdd0a292be6d46993
בר בן יוסף <bar180999@gmail.com>
2021-05-26 20:34:07 +0300

    Add conctact up Model.

---
+++ DGN/Models/ContactUs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGN.Models
+{
+    public class ContactUs
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Branch name")]
+        public string BranchName { get; set; }
+
+        [DisplayName("Address")]
+        public Location BranchLocation { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        [DisplayName("Branch's Activity Time")]
+        public string ActivityTime { get; set; }
+    }
+}
---
+++ DGN/Models/Location.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DGN.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string BranchName { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public Location(int branchId, string name, string desc, double latitude, double longitude)
+        {
+            this.Id = branchId;
+            this.BranchName = name;
+            this.Description = desc;
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+    }
+
+    public class LocationsList
+    {
+        public IEnumerable<Location> LocationList { get; set; }
+    }
+}
