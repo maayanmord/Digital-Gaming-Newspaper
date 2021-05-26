a2cfa65f92d655106e4a6ef86e07c2e4e05d5148
בר בן יוסף <bar180999@gmail.com>
2021-05-26 20:38:24 +0300

    Edit Location props.

--- DGN/Models/Location.cs
+++ DGN/Models/Location.cs
@@ -5,16 +5,12 @@ namespace DGN.Models
     public class Location
     {
         public int Id { get; set; }
-        public string BranchName { get; set; }
-        public string Description { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
-        public Location(int branchId, string name, string desc, double latitude, double longitude)
+        public Location(int branchId, double latitude, double longitude)
         {
             this.Id = branchId;
-            this.BranchName = name;
-            this.Description = desc;
             this.Latitude = latitude;
             this.Longitude = longitude;
         }
