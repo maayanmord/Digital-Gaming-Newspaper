a665941092f842305e78307d99ec0481c448c32c
maayan mordehai <maayanmordehai3@gmail.com>
2021-06-06 21:20:47 +0300

    ordered in functions

--- DGN/Controllers/UsersController.cs
+++ DGN/Controllers/UsersController.cs
@@ -31,7 +31,7 @@ namespace DGN.Controllers
             return View(await _context.User.ToListAsync());
         }
 
-        // GET: Users/Details/5
+        // GET: Users/Profile/5
         [Authorize]
         public async Task<IActionResult> Profile(int? id)
         {
@@ -65,16 +65,15 @@ namespace DGN.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(string Email, string password)
-        {
-            if (_context.User.Where<User>(u => u.Email == Email).ToList<User>().Count != 1)
+        {            
+            if (!EmailExists(Email))
             {
                 ViewData["Error"] = "The email or password is incorrect";
                 Response.StatusCode = (int)HttpStatusCode.Unauthorized;
             }
             else {
-                User usr = _context.User.Where(u => u.Email == Email).FirstOrDefault();
-                Password pwd = _context.Password.Where(p => p.UserId == usr.Id).FirstOrDefault();
-                if (pwd.Check(password))
+                User usr = await _context.User.Include(u => u.Password).FirstOrDefaultAsync(u => u.Email == Email);
+                if (usr.Password.Check(password))
                 {
                     List<Claim> claims = new List<Claim>
                     {
@@ -118,56 +117,39 @@ namespace DGN.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register([Bind("Id,Email,Username,Firstname,Lastname,Birthday,Role,ImageLocation,About")] User user, string plainPass, string confirmPass)
         {
-            if (!ValidPass(plainPass))
-            {
-                ViewData["Error"] = "The minumum requierments are: 8 characters long containing 1 uppercase letter, 1 lowercase letter, a number and a special character";
-                Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            }
-            if (!plainPass.Equals(confirmPass)) 
-            {
-                ViewData["Error"] = "Passwords does not match";
-                Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            }
-            if (_context.User.Where<User>(u => u.Username == user.Username).ToList<User>().Count != 0)
+            if (UsernameExists(user.Username))
             {
                 ModelState.AddModelError("Username", "Username is aleardy exists");
                 Response.StatusCode = (int)HttpStatusCode.Conflict;
             }
-            if (_context.User.Where<User>(u => u.Email == user.Email).ToList<User>().Count != 0)
+            if (EmailExists(user.Email))
             {
-                ModelState.AddModelError("Email", "This mail address is aleady in use");
+                ModelState.AddModelError("Email", "This Email address is aleady in use");
                 Response.StatusCode = (int)HttpStatusCode.Conflict;
             }
-            if (ModelState.IsValid)
+            if (CanUsePassword(plainPass, confirmPass) && ModelState.IsValid)
             {
-                Password userPass = new Password(user.Id, plainPass, user);
-                user.Password = userPass;
+                user.Password = new Password(user.Id, plainPass, user);
                 _context.Add(user);
-                _context.Add(userPass);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(user);
         }
 
-        private bool ValidPass(string plainPass)
-        {
-            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
-            return regex.IsMatch(plainPass);
-        }
-
         // GET: Users/ChangePassword/5
         [Authorize]
         public async Task<IActionResult> ChangePassword(int? id)
         {
-            string usersid = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            string userSid = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             string userRole = HttpContext.User.FindFirst(ClaimTypes.Role)?.Value;
 
             if (id == null)
             {
                 return NotFound();
             }
-            if ((!usersid.Equals(id.ToString())) && (!userRole.Equals(UserRole.Admin.ToString())))
+            // Only when the user tring to change his own password
+            if (!userSid.Equals(id.ToString()))
             {
                 Response.StatusCode = (int)HttpStatusCode.Unauthorized;
                 return Unauthorized();
@@ -186,55 +168,48 @@ namespace DGN.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ChangePassword(int id, string currentPassword, string newPassword, string confirmNewPassword)
+        [Authorize]
+        public async Task<IActionResult> ChangePassword(int? id, string currentPassword, string newPassword, string confirmNewPassword)
         {
             if (id == null)
             {
                 return NotFound();
             }
+
             // Getting current user info from cookies
-            string usersid = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            string userSid = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             string userRole = HttpContext.User.FindFirst(ClaimTypes.Role)?.Value;
-            // If current user trying to replace password to diffrent user and he is not Admin
-            if ((!usersid.Equals(id.ToString())) && (!userRole.Equals(UserRole.Admin.ToString())))
+
+            // Only when the user tring to change his own password
+            if (!userSid.Equals(id.ToString()))
             {
                 Response.StatusCode = (int)HttpStatusCode.Unauthorized;
                 return Unauthorized();
             }
 
             // Getting the user and password info from db
-            User user = _context.User.Find(id);
-            Password password = _context.Password.Where<Password>(p => p.UserId == id).FirstOrDefault();
+            User user = await _context.User.Include(u => u.Password).FirstOrDefaultAsync(u => u.Id == id);
 
             // Checking password is correct
-            if (!password.Check(currentPassword)) {
+            if (!user.Password.Check(currentPassword)) {
                 ViewData["Error"] = "Password is not currect";
                 Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 return View();
             }
-            // Making sure new password is valid
-            if (!ValidPass(newPassword))
-            {
-                ViewData["Error"] = "The minumum requierments are: 8 characters long containing 1 uppercase letter, 1 lowercase letter, a number and a special character";
-                Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                return View();
+            if (CanUsePassword(newPassword, confirmNewPassword)) {
+                // Creating the new password
+                user.Password = new Password(user.Id, newPassword, user);
+                _context.User.Update(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            // Making sure new password confirmed
-            if (!newPassword.Equals(confirmNewPassword))
-            {
-                ViewData["Error"] = "Passwords does not match";
-                Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                return View();
-            }
-            // Creating the new password
-            Password p = new Password(id, newPassword, user);
-            _context.Password.Remove(password);
-            _context.Password.Add(p);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return View();
         }
 
         // GET: Users/Delete/5
+        // TODO: DELETE USER ONLY IF I AM CURRENT USER AND CONFIRMD PASSWORD
+        // OR WHEN ADMIN WITHOUT CONFIRM - ADMIN PASS CONFIRMD - NEW FUNC
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -253,8 +228,11 @@ namespace DGN.Controllers
         }
 
         // POST: Users/Delete/5
+        // TODO: DELETE USER ONLY IF I AM CURRENT USER AND CONFIRMD PASSWORD
+        // OR WHEN ADMIN WITHOUT CONFIRM - ADMIN PASS CONFIRMD - NEW FUNC
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var user = await _context.User.FindAsync(id);
@@ -266,6 +244,39 @@ namespace DGN.Controllers
         private bool UserExists(int id)
         {
             return _context.User.Any(e => e.Id == id);
+        }
+
+        private bool EmailExists(string Email)
+        {
+            return _context.User.Any(e => e.Email == Email);
+        }
+
+        private bool UsernameExists(string Username)
+        {
+            return _context.User.Any(e => e.Username == Username);
+        }
+        private bool ValidPass(string plainPass)
+        {
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+            return regex.IsMatch(plainPass);
+        }
+
+        private bool CanUsePassword(string password, string confirmPassword) {
+            // Making sure new password is valid
+            if (!ValidPass(password))
+            {
+                ViewData["Error"] = "The minumum requierments are: 8 characters long containing 1 uppercase letter, 1 lowercase letter, a number and a special character";
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return false;
+            }
+            // Making sure new password confirmed
+            if (!password.Equals(confirmPassword))
+            {
+                ViewData["Error"] = "Passwords does not match";
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return false;
+            }
+            return true;
         }
     }
 }
