80642f0a373f76e42ef8f54b42fdd62c78c78e9e
בר בן יוסף <bar180999@gmail.com>
2021-07-10 15:58:14 +0300

    FIX: password validation.

---
+++ DGN/wwwroot/js/changePassword.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('#newPassword, #confirmNewPassword').on('keyup', function () {
+        if ($('#newPassword').val() == $('#confirmNewPassword').val()) {
+            $('#message').html('');
+            $('#submit').prop('disabled', false)
+        } else {
+            $('#message').html('Passwords Not Matching').css('color', 'red');
+            $('#submit').prop('disabled', true)
+        }
+    });
+})
--- DGN/wwwroot/js/password.js
+++
@@ -1,6 +0,0 @@
-﻿$('#newPassword, #confirmNewPassword').on('keyup', function () {
-    if ($('#newPassword').val() == $('#confirmNewPassword').val()) {
-        $('#message').html('');
-    } else
-        $('#message').html('Passwords Not Matching').css('color', 'red');
-});
