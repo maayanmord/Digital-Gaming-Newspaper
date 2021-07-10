8316614e1553d6961f40ab82204e7fed7b304a17
בר בן יוסף <bar180999@gmail.com>
2021-07-10 15:09:20 +0300

    Cheack id Password and Confirm password match

---
+++ DGN/wwwroot/js/password.js
@@ -0,0 +1,6 @@
+﻿$('#newPassword, #confirmNewPassword').on('keyup', function () {
+    if ($('#newPassword').val() == $('#confirmNewPassword').val()) {
+        $('#message').html('');
+    } else
+        $('#message').html('Passwords Not Matching').css('color', 'red');
+});
