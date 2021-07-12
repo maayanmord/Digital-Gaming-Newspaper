06a3853ee30cecd296a7aba60e1d8090a3745ec1
בר בן יוסף <bar180999@gmail.com>
2021-07-12 19:59:31 +0300

    Add password and confitm password as required filed - in register.

---
+++ DGN/wwwroot/js/register.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    var passwordsMatch = false;
+    var passwordNOTEmpty = false;
+    var confPasswordNOTEmpty = false;
+
+    $('#Password, #confirmPassword').on('keyup', function () {
+        if ($('#Password').val() == $('#confirmPassword').val()) {
+            $('#notMatchPassMessage').html('');
+            passwordsMatch = true;
+        } else {
+            $('#notMatchPassMessage').html('Passwords Not Matching').css('color', 'red');
+            passwordsMatch = false;
+        }
+
+        if ($('#Password').val() != '') {
+            $('#passMessage').html('');
+            passwordNOTEmpty = true;
+        } else {
+            $('#passMessage').html('You forget to enter the password!').css('color', 'red');
+            passwordNOTEmpty = false;
+        }
+
+        if ($('#confirmPassword').val() != '') {
+            $('#confPassMessage').html('');
+            confPasswordNOTEmpty = true;
+        } else {
+            $('#confPassMessage').html('Please enter the password again!').css('color', 'red');
+            confPasswordNOTEmpty = false;
+        }
+
+        if (passwordsMatch && passwordNOTEmpty && confPasswordNOTEmpty) {
+            $('#submit').prop('disabled', false)
+        } else {
+            $('#submit').prop('disabled', true)
+        }
+    });
+})
