16853e6f7d679ec35bc34f07d8930a51cb6e9caa
בר בן יוסף <bar180999@gmail.com>
2021-07-17 15:15:40 +0300

    FIX: disable submit button when needed.

--- DGN/wwwroot/js/changePassword.js
+++ DGN/wwwroot/js/changePassword.js
@@ -1,11 +1,31 @@
 ﻿$(function () {
-    $('#newPassword, #confirmNewPassword').on('keyup', function () {
+    var passwordMatch = false;
+    var passwordNOTEmpty = false;
+
+    $('#newPassword, #confirmNewPassword, #currPassword').on('keyup', function () {
         if ($('#newPassword').val() == $('#confirmNewPassword').val()) {
-            $('#message').html('');
-            $('#submit').prop('disabled', false)
+            passwordMatch = true;
         } else {
-            $('#message').html('Passwords Not Matching').css('color', 'red');
+            passwordMatch = false;
+        }
+
+        if ($('#newPassword').val() == '' || $('#confirmNewPassword').val() == '' || $('#currPassword').val() == '' ) {
+            passwordNOTEmpty = false;
+        } else {
+            passwordNOTEmpty = true;
+        }
+
+        if (passwordMatch && passwordNOTEmpty) {
+            $('#submit').prop('disabled', false)
+            $('#message').html('');
+        } else {
             $('#submit').prop('disabled', true)
+            if (!passwordMatch) {
+                $('#message').html('Passwords Not Matching').css('color', 'red');
+            } else {
+                $('#message').html('Some fields are missing!').css('color', 'red');
+            }
+            
         }
     });
 })
