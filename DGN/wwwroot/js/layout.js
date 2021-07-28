9e4c154cdd95aa0bf98da1993e1bd132939b67ac
Ran Biderman <bsuro10@gmail.com>
2021-07-28 21:31:43 +0300

    Draw the logo with canvas

--- DGN/wwwroot/js/layout.js
+++ DGN/wwwroot/js/layout.js
@@ -1,14 +1,21 @@
 ﻿"use strict";
 
-// Toggle .header-scrolled class to #header when page is scrolled
-$(window).scroll(() => {
-    if ($(this).scrollTop() > 50) {
+$(function () {
+    /*var c = document.getElementById("logoCanvas");*/
+    var ctx = $("#logoCanvas").get(0).getContext("2d");
+    var img = $("#logo").get(0);
+    ctx.drawImage(img, 50, 30, 170, 93);
+
+    // Toggle .header-scrolled class to #header when page is scrolled
+    $(window).scroll(() => {
+        if ($(this).scrollTop() > 50) {
+            $('#header').addClass('header-scrolled');
+        } else {
+            $('#header').removeClass('header-scrolled');
+        }
+    });
+
+    if ($(window).scrollTop() > 50) {
         $('#header').addClass('header-scrolled');
-    } else {
-        $('#header').removeClass('header-scrolled');
     }
 });
-
-if ($(window).scrollTop() > 50) {
-    $('#header').addClass('header-scrolled');
-}
