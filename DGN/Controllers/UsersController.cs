b31f1e8cdd4e00e0c42fafe99e836885a48147b7
maayan mordehai <maayanmordehai3@gmail.com>
2021-08-02 20:58:08 +0300

    fixed password bug

--- DGN/Controllers/UsersController.cs
+++ DGN/Controllers/UsersController.cs
@@ -234,7 +234,9 @@ namespace DGN.Controllers
             if (CanUsePassword(newPassword, confirmNewPassword))
             {
                 // Creating the new password
-                user.Password = new Password(user.Id, newPassword, user);
+                Password newPass = new Password(user.Id, newPassword, user);
+                user.Password.Hash = newPass.Hash;
+                user.Password.Salt = newPass.Salt;
                 _context.User.Update(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Profile), new { id = user.Id });
